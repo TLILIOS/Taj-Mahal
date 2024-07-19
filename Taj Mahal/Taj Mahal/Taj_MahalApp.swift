f88d2b3c7d0e62e01d0db50ff1ea06a0e9004ba5
TLiLi HAMDI <tlilios@yahoo.com>
2024-07-19 12:28:10 +0200

    Starting

---
+++ Taj Mahal/Taj Mahal/Taj_MahalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Taj_MahalApp.swift
+//  Taj Mahal
+//
+//  Created by HTLILI on 19/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct Taj_MahalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
